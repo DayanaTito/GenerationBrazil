432121847bdb362076397ff495d2e0263168f4c7
Dayana Tito <Dayanatitochoque@hotmail.com>
2022-04-09 01:24:15 -0300

    Exercícios

--- Java/POO/2.ExPOO-Herança/Ex03Loja-Collections/ExLoja.java
+++ Java/POO/2.ExPOO-Herança/Ex03Loja-Collections/ExLoja.java
@@ -9,27 +9,27 @@ public class ExLoja {
 		public static void main(String[] args) {
 
 
-			String produto1 ="Caderno";
-			String produto2 = "Caneta";
-			String produto3 ="Lápis";
+			String p1 ="Celular";
+			String p2 = "Televisão";
+			String p3 ="Tablet";
 			
 			ArrayList<String> produtos = new ArrayList<>();
-			produtos.add(produto1);
-			produtos.add(produto2);
-			produtos.add(produto3);
+			produtos.add(p1);
+			produtos.add(p2);
+			produtos.add(p3);
 			
 			System.out.println("Lista de produtos: = "+ produtos);
 			
 			Scanner leia = new Scanner(System.in);
 			System.out.println("Voce deseja adicionar ou remover?" + "\n1 - Adicionar"+ " \n2 - Remover");
-			int num= leia.nextInt();
+			int r= leia.nextInt();
 			
-			if(num == 1){
+			if(r == 1){
 					System.out.println("Nome do produto: ");
 					String produto = leia.next();
 					produtos.add(produto);
 			}
-			else if(num == 2){
+			else if(r == 2){
 					System.out.println("Qual produto você gostaria de remover:");
 					int quantidade = produtos.size();
 				//	System.out.println(quantidade);
@@ -37,8 +37,8 @@ public class ExLoja {
 						System.out.println((i + 1) +produtos.get(i));
 					}
 					System.out.println("Digite o número correspondente ao produto: ");
-					int numProduto = leia.nextInt();
-					produtos.remove(numProduto - 1);
+					int rProduto = leia.nextInt();
+					produtos.remove(rProduto - 1);
 					System.out.println();
 					System.out.println("A nova lista é: " + produtos);
 			}
